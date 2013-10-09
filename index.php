10ee51b49ae92d06640d1fe2f81b4a0f13c428c5
Marcus Battle <marcus@marcusbattle.com>
2013-10-09 03:23:13 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link rel="stylesheet" type="text/css" href="assets/css/platform.css" />
+	</head>
+	<body>
+		<section class="header">
+			<h1 class="logo"><img src="assets/img/summit-web-horiz-white.png"></h1>
+			<ul class="nav">
+				<li><a href="">Home</a></li>
+				<li><a href="#theplatform">The Platform</a></li>
+				<li><a href="#auditions">Auditions</a></li>
+				<li><a href="#schedule">Schedule</a></li>
+				<li><a href="#rules">FAQs</a></li>
+			</ul>
+		</section>
+		<section class="intro">
+			<div class="slide">
+				<img src="assets/img/the-platform-logo.png" height="350px" />
+			</div>
+			<div class="countdown">
+				<span class="message">Online Auditions begin in</span>
+				<ul class="timer">
+					<li>
+						<span class="hours time">0</span>
+						<span>Hours</span>
+					</li>
+					<li>
+						<span class="minutes time">0</span>
+						<span>Minutes</span>
+					</li>
+					<li>
+						<span class="seconds time">0</span>
+						<span>Seconds</span>
+					</li>
+				</ul>
+			</div>
+		</section>
+		<section id="theplatform">
+			<div class="container">
+				<h2>What is The Platform?</h2>
+				<p>Next section</p>
+			</div>
+		</section>
+		<section id="auditions">
+			<div class="container">
+				<h2>Auditions</h2>
+				<p>Next section</p>
+			</div>
+		</section>
+		<section id="schedule">
+			<div class="container">
+				<h2>Schedule</h2>
+				<p>Next section</p>
+			</div>
+		</section>
+		<section id="faqs">
+			<div class="container">
+				<h2>FAQs</h2>
+				<p>Next section</p>
+			</div>
+		</section>
+		<section id="footer">
+			<div class="container">
+				<p>&copy; 2013 The Summit College Ministry. All Rights Reserved.</p>
+			</div>
+		</section>
+	</body>
+</html>
