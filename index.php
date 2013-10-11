fc1d4155774168c7d058099dbdcc71beadc5d1cf
Marcus Battle <marcus@marcusbattle.com>
2013-10-11 15:35:44 -0400

    added google analytics

--- index.php
+++ index.php
@@ -119,3 +119,18 @@
 		<script type="text/javascript" src="<?php echo get_bloginfo('template_url'); ?>/assets/js/platform.js"></script>
 	</body>
 </html>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-26588142-4']);
+  _gaq.push(['_setDomainName', 'campusunity.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
